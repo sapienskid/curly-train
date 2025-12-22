594468c88932e4c86281f9a0cc4a5922c7c18fb7
sapienskid <savinpokharel@gmail.com>
2025-12-22 16:38:13 +0545

    add paper and class outline

---
+++ mew.py
@@ -0,0 +1,20 @@
+class MEW:
+    def __init__(self, key_size=32):
+        pass
+    def _generate_key_matrix(self):
+        pass
+    def _get_move(self,byte_val):
+        pass
+    def _move(self):
+        pass
+    def _unmove(self):
+        pass
+    def _pass_encrypt(self)
+        pass
+    def _pass_decrypt(self)
+        pass
+    def encrypt(self):
+        pass
+    def decrypt(self):
+        pass
+        
